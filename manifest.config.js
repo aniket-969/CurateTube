648d6a9781bbaa9d38adf9ab7b8227b79a86b849
Aniket <aniketbaranwal8090@gmail.com>
2026-08-03 21:45:17 +0530

    Added manifest config and initial setup

---
+++ manifest.config.js
@@ -0,0 +1,35 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export default defineManifest({
+  manifest_version: 3,
+
+  name: "CurateTube",
+  version: "0.1.0",
+
+  action: {
+    default_popup: "index.html",
+  },
+
+  icons: {
+    "16": "icons/icon16.png",
+    "32": "icons/icon32.png",
+    "48": "icons/icon48.png",
+    "128": "icons/icon128.png",
+  },
+
+  permissions: [
+    "identity",
+    "storage",
+  ],
+
+  host_permissions: [
+    "https://www.googleapis.com/*",
+  ],
+
+  oauth2: {
+    client_id: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+    scopes: [
+      "https://www.googleapis.com/auth/youtube"
+    ]
+  }
+});
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+   <div className = "bg-blue-400">
+    Hei
+   </div>
+  )
+}
+
+export default App
