aee359711b4e5aaeb16e1ff3fe27f2c0aa660805
Aniket <aniketbaranwal8090@gmail.com>
2026-08-03 23:35:04 +0530

    Added oauth config and client id

--- manifest.config.js
+++ manifest.config.js
@@ -10,26 +10,20 @@ export default defineManifest({
     default_popup: "index.html",
   },
 
-  icons: {
-    "16": "icons/icon16.png",
-    "32": "icons/icon32.png",
-    "48": "icons/icon48.png",
-    "128": "icons/icon128.png",
-  },
+  //   icons: {
+  //     "16": "icons/icon16.png",
+  //     "32": "icons/icon32.png",
+  //     "48": "icons/icon48.png",
+  //     "128": "icons/icon128.png",
+  //   },
 
-  permissions: [
-    "identity",
-    "storage",
-  ],
+  permissions: ["identity", "storage"],
 
-  host_permissions: [
-    "https://www.googleapis.com/*",
-  ],
+  host_permissions: ["https://www.googleapis.com/*"],
 
   oauth2: {
-    client_id: "YOUR_CLIENT_ID.apps.googleusercontent.com",
-    scopes: [
-      "https://www.googleapis.com/auth/youtube"
-    ]
-  }
+    client_id:
+      "122698212064-erds8d2qht5snctlfhsdd0hs8njjgnt6.apps.googleusercontent.com",
+    scopes: ["https://www.googleapis.com/auth/youtube"],
+  },
 });
